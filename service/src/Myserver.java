e9eecdb0100f304787b4ba966fe189687594a1ab
andrewtyv <132301547+andrewtyv@users.noreply.github.com>
2024-07-05 19:20:40 +0200

    httpserver

--- service/src/Myserver.java
+++ service/src/Myserver.java
@@ -8,6 +8,15 @@ import java.net.Socket;
 public class Myserver implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type");
+
+        if ("OPTIONS".equals(exchange.getRequestMethod())) {
+            exchange.sendResponseHeaders(204, -1); // No Content for pre-flight request
+            return;
+        }
+
         String response = "Hello, World!";
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
